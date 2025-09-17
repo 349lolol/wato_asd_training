198e0cf4d53035511e816c7b593b01ee08ae9d22
349lolol <PatrickZX.wei@gmail.com>
2025-09-16 23:02:05 -0400

    subscribed to lidar topci

--- src/robot/costmap/src/costmap_node.cpp
+++ src/robot/costmap/src/costmap_node.cpp
@@ -7,6 +7,19 @@ CostmapNode::CostmapNode() : Node("costmap"), costmap_(robot::CostmapCore(this->
   // Initialize the constructs and their parameters
   string_pub_ = this->create_publisher<std_msgs::msg::String>("/test_topic", 10);
   timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&CostmapNode::publishMessage, this));
+
+  lidar_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+        "/lidar",  // topic name from gazebo bridge
+        10,        // QoS depth
+        std::bind(&CostmapNode::lidarCallback, this, std::placeholders::_1));
+}
+
+void CostmapNode::lidarCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+  // Process the incoming Lidar data
+  RCLCPP_INFO(this->get_logger(), "Received Lidar scan with:");
+  RCLCPP_INFO(this->get_logger(), "Angle range: [%f, %f]", msg->angle_min, msg->angle_max);
+  RCLCPP_INFO(this->get_logger(), "Angular resolution: %f", msg->angle_increment);
+  RCLCPP_INFO(this->get_logger(), "Number of ranges: %zu", msg->ranges.size());
 }
  
 // Define the timer to publish a message every 500ms
