42f1f98eb8937564afbc0c20f970ebea57268325
349lolol <PatrickZX.wei@gmail.com>
2025-09-21 21:54:57 -0400

    constructor and helper methods

--- src/robot/control/src/control_node.cpp
+++ src/robot/control/src/control_node.cpp
@@ -1,6 +1,28 @@
 #include "control_node.hpp"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2/LinearMath/Matrix3x3.h"
+#include <cmath>
 
-ControlNode::ControlNode(): Node("control"), control_(robot::ControlCore(this->get_logger())) {}
+ControlNode::ControlNode(): Node("control"), control_(robot::ControlCore(this->get_logger())) {
+  path_sub_ = this->create_subscription<nav_msgs::msg::Path>(
+      "/path", 10, [this](const nav_msgs::msg::Path::SharedPtr msg) {current_path_ = msg;});
+  odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+      "/odom/filtered", 10, [this](const nav_msgs::msg::Odometry::SharedPtr msg) {current_odom_ = msg;});
+  cmd_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+  timer_ = this->create_wall_timer(std::chrono::milliseconds(dt_ms), [this]() { controlLoop(); });
+}
+
+double ControlNode::computeDistance(const geometry_msgs::msg::Point& p1, const geometry_msgs::msg::Point& p2) {
+  return std::hypot(p1.x - p2.x, p1.y - p2.y);
+}
+
+double ControlNode::extractYawFromQuaternion(const geometry_msgs::msg::Quaternion& q) {
+  tf2::Quaternion quat(q.x, q.y, q.z, q.w);
+  tf2::Matrix3x3 m(quat);
+  double roll, pitch, yaw;
+  m.getRPY(roll, pitch, yaw);
+  return yaw;
+}
 
 int main(int argc, char ** argv)
 {
